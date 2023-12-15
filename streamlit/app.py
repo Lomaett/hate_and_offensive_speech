33cbcb8d89c5a7267414f9f338e3fd63d8fdf087
Ajirioghene Oguh <ajirioguh@gmail.com>
2023-12-15 21:06:40 +0100

    streamlit app finalised

--- streamlit/app.py
+++ streamlit/app.py
@@ -7,9 +7,9 @@ st.set_page_config(page_title="Tweet Classifer", page_icon=":cloud:", layout="wi
 bg_img = """
 <style>
 [data-testid="stAppViewContainer"] {
-background-image: url('https://images.unsplash.com/photo-1605778336817-121ba9819b96?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1282&q=80');
-background-size: cover;
-background-position: top left;
+background-image: url('https://images.unsplash.com/photo-1649389966541-12af5cb7efa0?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+background-size: 100% 100%;
+background-repeat: no-repeat;
 }
 
 [data-testid="stHeader"] {
@@ -21,8 +21,8 @@ right: 2rem;
 }
 
 [data-testid="stSidebar"] {
-background-image: url('https://images.unsplash.com/photo-1610270197941-925ce9015c40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80');
-background-size: center;
+background-image: url('https://images.unsplash.com/photo-1616945459385-6d4248514cc7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+background-size: cover;
 background-position: center;
 }
 </style>
@@ -39,8 +39,8 @@ def main():
 
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
-	st.title(":green[SynapseAI] Tweet Classifer")
-	st.subheader("Climate change tweet classification")
+	st.title(":red[Block]IT")
+	st.subheader("Hate Speech or Offensive Tweet Identifier")
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
@@ -51,14 +51,41 @@ def main():
 	if selection == "Information":
 		st.info("General Information")
 		# You can read a markdown file from supporting resources folder
-		st.markdown("""Welcome to SynapseAI, a cutting-edge AI company revolutionizing industries worldwide.
-		Our team of experts leverages advanced algorithms, machine learning, and natural language processing
-		to deliver innovative solutions. From personalized virtual assistants to data analytics and automation,
-		we empower businesses to thrive in the digital era. Join us on this transformative journey.
-		Our ML model for sentiment analysis of climate change-related tweets combines the power of natural
-		language processing and machine learning techniques to provide a comprehensive understanding of public
-		sentiments. By leveraging this model, we can extract meaningful insights from the vast pool of Twitter
-		data, enabling a data-driven approach towards addressing climate change and fostering informed decision-making.""")
+		st.markdown("""Welcome to BlockIT, your powerful tool for promoting positive online interactions!
+			  In a world where communication happens at the speed of a tweet, it's essential to foster a digital environment that is respectful and inclusive.
+			  Our app employs cutting-edge technology to analyze and classify tweets, helping users identify and understand the tone of online content.
+
+**Key Features:**
+
+1. **Hate Speech Detection:**
+   Uncover harmful language and discriminatory content. Our app utilizes advanced algorithms to identify tweets containing hate speech, promoting a safer and more inclusive digital space.
+
+2. **Offensive Language Recognition:**
+   Stay ahead of negativity by detecting offensive language in tweets. We strive to create a platform where users can express themselves without fear of encountering offensive content.
+
+3. **Neutral Content Classification:**
+   Not all tweets are harmful. Our app also distinguishes and categorizes tweets as neutral, ensuring that users can focus on engaging with content that contributes positively to the online discourse.
+
+**How It Works:**
+
+- **Real-time Analysis:**
+  Get instant feedback on the tweets you encounter. Our app works seamlessly to classify content as it appears in your feed, providing real-time insights into the nature of the tweets.
+
+- **User-Friendly Interface:**
+  BlockIT is designed with simplicity in mind. With an intuitive interface, users can effortlessly navigate through the app, making it easy to understand and utilize its features.
+
+- **Personalized Settings:**
+  Tailor the app to your preferences. Adjust settings to match your comfort level, allowing you to strike the right balance between free expression and a respectful online environment.
+
+**Why BlockIT?**
+
+- **Community Well-being:**
+  By using BlockIT, you contribute to a digital community that values respect and understanding. Together, we can make social media a space where everyone feels safe to express themselves.
+
+- **Empowerment Through Awareness:**
+  Knowledge is power. BlockIT empowers users by providing insights into the content they consume, fostering a sense of awareness and responsibility in online interactions.
+
+Join us in creating a positive online experience. Download BlockIT now and take control of your digital environment! Together, let's build a more inclusive and respectful online community.""")
 
 	# Building out the predication page
 	if selection == "Prediction":
@@ -97,9 +124,9 @@ def main():
 	if selection == "Credits":
 		st.info('Credits')
 		st.subheader('Author:')
-		st.text("""Ajirioghene Oguh""")
+		st.text("Ajirioghene Oguh")
 		st.subheader('Images')
-		st.text("""Background Image\t\tChristian Lue (unsplash.com)\n\nSidebar Image\t\t\tJohn Cameron (unsplash.com)""")
+		st.text("""Background Image\t\tISTOCK / GETTY IMAGES PLUS \n\nSidebar Image\t\t\tJason Leung (unsplash.com)""")
 
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
